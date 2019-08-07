e985d09dfca83f3d3b354d953882ee5439c8ab73
Subrat Shah <subratshah@users.noreply.github.com>
2019-08-07 09:37:09 +0530

    Test 1

---
+++ TestProgram/app/src/main/java/sample/com/testprogram/Echo.java
@@ -0,0 +1,7 @@
+package sample.com.testprogram;
+
+public class Echo {
+    public String ping(String message) {
+        return "Echo:" + message;
+    }
+}
---
+++ TestProgram/app/src/test/java/sample/com/testprogram/EchoTest.java
@@ -0,0 +1,23 @@
+package sample.com.testprogram;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class EchoTest {
+
+    private Echo subject;
+    private String message = "Starfish";
+
+    @Before
+    public void setup() {
+        subject = new Echo();
+
+    }
+
+    @Test
+    public void messageStarfish_ping_returnsEchoStarfish() {
+        assertEquals("Echo:" + message, subject.ping(message));
+    }
+}
