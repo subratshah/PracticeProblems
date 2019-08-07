c1ef21f9694985c0a47327843aa5891ad88a36b8
Subrat Shah <subratshah@users.noreply.github.com>
2019-08-07 09:53:26 +0530

    Test 3

--- TestProgram/app/src/main/java/sample/com/testprogram/Echo.java
+++ TestProgram/app/src/main/java/sample/com/testprogram/Echo.java
@@ -2,10 +2,12 @@ package sample.com.testprogram;
 
 public class Echo {
     public String ping(String message) {
-        if (message != null) {
-            return "Echo:" + message;
-        } else {
+        if (message == null || message.isEmpty()) {
             return "There is nothing to echo";
+        } else if (message.equals(message.toUpperCase())) {
+            return "WARNING:" + message;
+        } else {
+            return "Echo:" + message;
         }
     }
 }
--- TestProgram/app/src/test/java/sample/com/testprogram/EchoTest.java
+++ TestProgram/app/src/test/java/sample/com/testprogram/EchoTest.java
@@ -8,21 +8,24 @@ import static org.junit.Assert.assertEquals;
 public class EchoTest {
 
     private Echo subject;
-    private String message = "Starfish";
 
     @Before
     public void setup() {
         subject = new Echo();
-
     }
 
     @Test
     public void messageStarfish_ping_returnsEchoStarfish() {
-        assertEquals("Echo:" + message, subject.ping(message));
+        assertEquals("Echo:Starfish", subject.ping("Starfish"));
     }
 
     @Test
     public void messageNull_ping_returnsReplacementMessage() {
-        assertEquals("There is nothing to echo", subject.ping(null));
+        assertEquals("There is nothing to echo", subject.ping(""));
+    }
+
+    @Test
+    public void messageUppercase_ping_returnsWarning() {
+        assertEquals("WARNING:SHARK", subject.ping("SHARK"));
     }
 }
